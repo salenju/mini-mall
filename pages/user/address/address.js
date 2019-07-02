9a22dd7a25e4d7a712a5763ab02f4ec5249c4a6d
ju.dc <judc@bwoil.com>
2019-07-02 20:00:40 +0800

    添加收货地址和新增收获地址页面

--- pages/user/address/address.js
+++ pages/user/address/address.js
@@ -5,62 +5,60 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    addressList: [{
+        id: '269794988324487270',
+        address: '长富金茂大厦',
+        label: 'company',
+        labelName: '公司',
+        defaultAddress: false,
+        contactName: '琚先生',
+        contactNumber: '18565723173',
+        sex: 'male'
+      },
+      {
+        id: '269794988324487271',
+        address: '岗头发展大厦',
+        label: 'home',
+        labelName: '家',
+        defaultAddress: true,
+        contactName: '张女士',
+        contactNumber: '15221176843',
+        sex: 'female'
+      },
+      {
+        id: '269794988324487272',
+        address: '学府中路8号',
+        label: 'school',
+        labelName: '学校',
+        defaultAddress: false,
+        contactName: '王女士',
+        contactNumber: '15891365980',
+        sex: 'female'
+      },
+      {
+        id: '269794988324487273',
+        address: '喜洋洋超市',
+        label: 'parents',
+        labelName: '父母家',
+        defaultAddress: false,
+        contactName: '张先生',
+        contactNumber: '15891368888',
+        sex: 'male'
+      }
+    ],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  addAddress: function() {
+    wx.navigateTo({
+      url: '../newAddress/newAddress',
+    })
   }
+
 })
